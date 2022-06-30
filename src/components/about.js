94f3f8dd379207f93cc0dda30adb0b9abcb3be87
Fenil <fpatel7480@conestogac.on.ca>
2022-06-30 01:34:50 -0400

    Code for responsiveness added

--- src/components/about.js
+++ src/components/about.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import Navbar from './navbar'
 import Intro from './intro'
-import styles from '../styles/about.module.css'
 
 const About = () => {
     return (
-        <div className={styles.about}>
+        <>
             <Navbar />
             <Intro />
-        </div>
+        </>
     )
 }
 
