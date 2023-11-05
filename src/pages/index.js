4c6c11baa0449b3ed66f4a0337ca11c5e4c1d3fb
fenil <7fenilpatel@gmail.com>
2023-11-05 17:13:24 -0500

    Update to Arrow Fun.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,11 @@
-import Head from 'next/head'
-import About from '../components/about'
-import Skills from '../components/skills'
-import Projects from '../components/projects'
-import Contact from '../components/contact'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import About from '../components/about';
+import Skills from '../components/skills';
+import Projects from '../components/projects';
+import Contact from '../components/contact';
+import styles from '../styles/Home.module.css';
 
-
-export default function Home() {
+const Home = () => {
   return (
     <div className={styles.container}>
       <Head>
@@ -14,15 +13,18 @@ export default function Home() {
         <title>Fenil Patel | Portfolio site of web developer</title>
 
         {/* description with length between 130-160 characters */}
-        <meta name="description" content="Personal portfolio website of Fenil Patel. Fenil Patel is a Web Developer specializing in Frontend development. Looking for a  full-time job." />
+        <meta
+          name='description'
+          content='Personal portfolio website of Fenil Patel. Fenil Patel is a Web Developer specializing in Frontend development. Looking for a  full-time job.'
+        />
 
         {/* robots meta tag */}
-        <meta name="robots" content="index, follow" />
+        <meta name='robots' content='index, follow' />
 
-        <meta charSet="UTF-8" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width,initial-scale=1.0" />
-        <link rel="icon" href='favicon.png' />
+        <meta charSet='UTF-8' />
+        <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
+        <meta name='viewport' content='width=device-width,initial-scale=1.0' />
+        <link rel='icon' href='favicon.png' />
       </Head>
 
       <main className={styles.main}>
@@ -32,5 +34,7 @@ export default function Home() {
         <Contact />
       </main>
     </div>
-  )
-}
+  );
+};
+
+export default Home;
