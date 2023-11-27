4adf6bb8bc160b9e21723330b588bcea440540ab
fenil <7fenilpatel@gmail.com>
2023-11-26 22:37:47 -0500

    Remove React Import

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,5 @@
-import React from 'react'
-
 const Layout = ({ children }) => {
-    return (
-        <>{children}</>
-    )
-}
+  return <>{children}</>;
+};
 
-export default Layout
+export default Layout;
