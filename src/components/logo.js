4a31ccfada5fb9b3dbaa90cc35075cb1bae09fad
fenil <7fenilpatel@gmail.com>
2023-11-30 23:04:17 -0500

    Remove React Import

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,11 +1,14 @@
-import React from 'react'
 import Link from 'next/link';
 import styles from '../styles/logo.module.css';
 
 const Logo = () => {
-    return (
-        <i className={styles.logo}><Link href="http://fenil.me" rel="noopener noreferrer">fenil.me</Link></i>
-    )
-}
+  return (
+    <i className={styles.logo}>
+      <Link href='http://fenil.me' rel='noopener noreferrer'>
+        fenil.me
+      </Link>
+    </i>
+  );
+};
 
-export default Logo
+export default Logo;
