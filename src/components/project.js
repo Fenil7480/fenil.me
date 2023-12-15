19ac724df9265c7a407715efb353d4be3b3b678c
fenil <7fenilpatel@gmail.com>
2023-12-14 23:22:54 -0500

    Remove React Import

--- src/components/project.js
+++ src/components/project.js
@@ -1,21 +1,43 @@
-import React from 'react'
-import Image from 'next/image'
-import seoDesktop from '../../public/seoDesktop.PNG'
-import styles from '../styles/project.module.css'
+import Image from 'next/image';
+import seoDesktop from '../../public/seoDesktop.PNG';
+import styles from '../styles/project.module.css';
 
 const Project = () => {
-    return (
-        <div className={`${styles.project} flex-row flex-start`}>
-            <div>
-                <a href="https://www.onpageseocheck.com/" target="_blank" rel='noreferrer'>
-                    <Image className={styles.img} src={seoDesktop} alt="onpageseocheck.com Image" width={600} height={350} /></a>
-            </div>
-            <div>
-                <a href="https://www.onpageseocheck.com/" target="_blank" rel='noreferrer'><h2 className={styles.title}>onpageseocheck.com</h2></a>
-                <h5 className={styles.des}>This tool helps you analyze your webpage&apos; s basic SEO and give tips to improve your SEO. This is SPA built using next.js. Javascript library cheerio is used for scrapping webpages. SSR of next.js is used. A utility-first CSS framework(Tailwind) is applied for giving a smooth CSS look.</h5>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className={`${styles.project} flex-row flex-start`}>
+      <div>
+        <a
+          href='https://www.onpageseocheck.com/'
+          target='_blank'
+          rel='noreferrer'
+        >
+          <Image
+            className={styles.img}
+            src={seoDesktop}
+            alt='onpageseocheck.com Image'
+            width={600}
+            height={350}
+          />
+        </a>
+      </div>
+      <div>
+        <a
+          href='https://www.onpageseocheck.com/'
+          target='_blank'
+          rel='noreferrer'
+        >
+          <h2 className={styles.title}>onpageseocheck.com</h2>
+        </a>
+        <h5 className={styles.des}>
+          This tool helps you analyze your webpage&apos; s basic SEO and give
+          tips to improve your SEO. This is SPA built using next.js. Javascript
+          library cheerio is used for scrapping webpages. SSR of next.js is
+          used. A utility-first CSS framework(Tailwind) is applied for giving a
+          smooth CSS look.
+        </h5>
+      </div>
+    </div>
+  );
+};
 
-export default Project
+export default Project;
